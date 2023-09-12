0a4823e08a3dafa2676d577a73083ac87d85506f
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-09-12 14:21:23 -0700

    Not print

---
+++ 3-quick_sort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void
+swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int
+lomuto_partition(int *array, int low, int high)
+{
+    int pivot = array[high];
+    int i = low - 1;
+    
+
+    for (int j = low; j <= high - 1; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
+}
+
+void
+quick_sort_recursive(int *array, int low, int high)
+{
+    if (low < high) {
+        int pivot_index = lomuto_partition(array, low, high);
+        quick_sort_recursive(array, low, pivot_index - 1);
+        quick_sort_recursive(array, pivot_index + 1, high);
+    }
+}
+
+void
+quick_sort(int *array, size_t size)
+{
+    
+    if (array == NULL || size <= 1) {
+        return;
+    }
+    quick_sort_recursive(array, 0, size - 1);
+}
