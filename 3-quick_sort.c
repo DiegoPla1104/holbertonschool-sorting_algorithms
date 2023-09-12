fc046bf2c5d1c1e479e5e9acdf409f3f30de00e3
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-09-12 14:25:00 -0700

    Not print

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -15,9 +15,10 @@ lomuto_partition(int *array, int low, int high)
 {
     int pivot = array[high];
     int i = low - 1;
+    int j;
     
 
-    for (int j = low; j <= high - 1; j++) {
+    for (j = low; j <= high - 1; j++) {
         if (array[j] <= pivot) {
             i++;
             swap(&array[i], &array[j]);
